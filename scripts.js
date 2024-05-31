2995975b53d87b0edb0d10004990928b02afafd2
Jiwon Jeong <jiwon.j.jeong@gmail.com>
2024-05-31 14:20:19 -0700

    Add book object constructor and add new books to array

---
+++ scripts.js
@@ -0,0 +1,27 @@
+const myLibrary=[];
+
+// book object constructor
+function Book(title, author, pages, isRead){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+
+    this.info = function(){
+        let infoText = this.title + " by " + this.author + ", " + this.pages + " pages, ";
+        if (isRead){
+            return infoText + "read.";
+        } else {
+            return infoText + "not read yet.";
+        }
+    }
+}
+
+function addBookToLibrary(bookToAdd) {
+    myLibrary.push(bookToAdd);
+}
+
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+addBookToLibrary(theHobbit);
+console.log(myLibrary);
+console.log(myLibrary[0].info());
